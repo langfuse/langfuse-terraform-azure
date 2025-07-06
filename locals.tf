1e1e29eca796d27eef8e712a88ddbcef34c1f7ed
anshmove37 <ansh@move37.ai>
2025-07-06 20:19:28 +1000

    feat: md5 hash for creating global unique prefix from domain

--- locals.tf
+++ locals.tf
@@ -1,6 +1,9 @@
 locals {
   tag_name = lower(var.name) == "langfuse" ? "Langfuse" : "Langfuse ${var.name}"
 
-  # Convert domain to globally unique name format, supporting only lowercase letters and numbers (e.g., company.com -> companycom)
-  globally_unique_prefix = replace(lower(var.domain), ".", "")
+  # Convert domain to globally unique name format, supporting only lowercase letters and numbers 
+  # Supports naming convention required by azure resources used for this deployment
+  # MD5 substr enables traceability for domains used
+  # lf3 prefix helps identify langfuse v3 resource
+  globally_unique_prefix = "lf3${substr(md5(var.domain), 0, 6)}"
 }
