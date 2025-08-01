5d7ee70226d7a2b9a8a44c55006eab7477f62bec
steffen911 <steffen@langfuse.com>
2025-08-01 11:25:12 +0200

    chore: bump langfuse helm to 1.3.4

--- variables.tf
+++ variables.tf
@@ -150,7 +150,7 @@ variable "use_ddos_protection" {
 variable "langfuse_helm_chart_version" {
   description = "Version of the Langfuse Helm chart to deploy"
   type        = string
-  default     = "1.3.3"
+  default     = "1.3.4"
 }
 
 variable "additional_env" {
