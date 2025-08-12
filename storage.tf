383b2b7d0eec95eb11beaf63bd6fba23ee0baa97
Tomasz Wisniewski <wi5nia@users.noreply.github.com>
2025-08-12 13:08:05 +0200

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -20,6 +20,7 @@ resource "azurerm_storage_account" "this" {
   account_replication_type = "GRS"
   min_tls_version          = "TLS1_2"
   public_network_access_enabled = false
+  allow_nested_items_to_be_public = false
 
   blob_properties {
     versioning_enabled = true
