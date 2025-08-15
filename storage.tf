9866a8bbfee9e1fce933e4af109e948748b6d475
Steffen Schmitz <steffen@langfuse.com>
2025-08-15 09:36:02 +0200

    chore: disallow public nested items

--- storage.tf
+++ storage.tf
@@ -13,13 +13,14 @@ resource "azurerm_subnet_nat_gateway_association" "storage" {
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = replace(module.naming.storage_account.name_unique, "-", "")
-  resource_group_name      = azurerm_resource_group.this.name
-  location                 = azurerm_resource_group.this.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  min_tls_version          = "TLS1_2"
-  public_network_access_enabled = false
+  name                            = replace(module.naming.storage_account.name_unique, "-", "")
+  resource_group_name             = azurerm_resource_group.this.name
+  location                        = azurerm_resource_group.this.location
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  min_tls_version                 = "TLS1_2"
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
 
   blob_properties {
     versioning_enabled = true
