0036902ce2cb2e35502890ef74eede1f6a8cf6e8
Steffen Schmitz <steffen@langfuse.com>
2025-08-15 09:36:31 +0200

    fix: restrict public network access to storage_account (#20)

--- storage.tf
+++ storage.tf
@@ -13,12 +13,14 @@ resource "azurerm_subnet_nat_gateway_association" "storage" {
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = replace(module.naming.storage_account.name_unique, "-", "")
-  resource_group_name      = azurerm_resource_group.this.name
-  location                 = azurerm_resource_group.this.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  min_tls_version          = "TLS1_2"
+  name                            = replace(module.naming.storage_account.name_unique, "-", "")
+  resource_group_name             = azurerm_resource_group.this.name
+  location                        = azurerm_resource_group.this.location
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  min_tls_version                 = "TLS1_2"
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
 
   blob_properties {
     versioning_enabled = true
@@ -26,6 +28,11 @@ resource "azurerm_storage_account" "this" {
     container_delete_retention_policy {
       days = 7
     }
+  }
+
+  network_rules {
+    default_action             = "Deny"
+    virtual_network_subnet_ids = [azurerm_subnet.storage.id]
   }
 }
 
