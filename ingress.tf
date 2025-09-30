32e5609ee0c1405ac91d4186cdb0ce29b3d2fcfd
Niklas Hölterhoff <31936483+nhoelterhoff@users.noreply.github.com>
2025-09-30 14:01:09 +0200

    fix: ignore `redirect_configuration` and `ssl_certificate changes` in Application Gateway lifecycle (#24)
    * fix: ignore changes on redirect configuration
    
    * fix: ignroe changes on ssl certificate

--- ingress.tf
+++ ingress.tf
@@ -214,6 +214,8 @@ resource "azurerm_application_gateway" "this" {
       probe,
       request_routing_rule,
       frontend_port,
+      redirect_configuration,
+      ssl_certificate,
     ]
   }
 }
