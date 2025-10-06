6c414ab25d350d509e63c0909e9d2217acd836c9
Niklas Hoelterhoff <31936483+nhoelterhoff@users.noreply.github.com>
2025-10-06 13:53:43 +0200

    fix: terraform fmt

--- langfuse.tf
+++ langfuse.tf
@@ -1,5 +1,5 @@
 locals {
-  langfuse_values   = <<EOT
+  langfuse_values       = <<EOT
 langfuse:
   salt:
     secretKeyRef:
@@ -52,7 +52,7 @@ s3:
   mediaUpload:
     prefix: "media/"
 EOT
-  encryption_values = var.use_encryption_key == false ? "" : <<EOT
+  encryption_values     = var.use_encryption_key == false ? "" : <<EOT
 langfuse:
   encryptionKey:
     secretKeyRef:
